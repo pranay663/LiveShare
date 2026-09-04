ca7310d68b11648bafa4d72e2bcf66830fe8058b
R Jaya Pranay Raju <25020103376@reva.edu.in>
2026-09-04 15:07:56 +0530

    Updating activity 3

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void greet(const char *name)
+{
+    printf("Hello, %s! welcome to your GitHub portfolio.\n", name);
+}
+
+int main()
+{
+    printf("Hello World!\n");
+
+    greet("Ada");
+
+    return 0;
+}
